b486448c152672eae9c79483502f01b511422fac
Divyanshu <divyanshu.it19@nsut.ac.in>
2022-02-06 19:56:01 +0530

    added partial layouts

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app= express();
 const port= 3000;
+const ejsLayout = require('express-ejs-layouts');
 
+app.use(ejsLayout);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts', true);
 //setup view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
