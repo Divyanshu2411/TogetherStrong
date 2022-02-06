0b324d5e59e9605c2199e0b8b3f19aaf1334bb77
Divyanshu <divyanshu.it19@nsut.ac.in>
2022-02-06 20:11:10 +0530

    Connected to Database

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app= express();
 const port= 3000;
 const ejsLayout = require('express-ejs-layouts');
-
+const db = require('./config/mongoose');
+//layout settings, remember the plurlal stuff
 app.use(ejsLayout);
 app.set('layout extractStyles',true);
 app.set('layout extractScripts', true);
+
+
 //setup view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
