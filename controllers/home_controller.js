82fb92ffd8ac36c596ef3ccf50e95fb666d0df0e
Divyanshu <divyanshu.it19@nsut.ac.in>
2022-04-11 10:00:31 +0530

    posts with user, learned prepopulating

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,26 @@
 const Post = require('../model/post');
 
 module.exports.home = function(req, res){
-    Post.find(function(err,post){
-        if(err){
-            console.log(err);
-        }
-        console.log(post);
+
+    //normal finding the user id
+    // Post.find(function(err,post){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     console.log(post);
+    //     return res.render('home',{
+    //         title: "Home",
+    //         posts:post
+    //     });
+    // });
+
+    //but we want to populate it with the user, so we will have to use the populate method that does another query to the db and populates the whole user document.
+
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home',{
             title: "Home",
-            posts:post
+            posts:posts
         });
-    });
+    })
         
     }
